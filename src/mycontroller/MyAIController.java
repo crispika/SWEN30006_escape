3a9a10b4d8f7c6e8458fae892b6296a2f7f31d59
Olivia Wang <eliyaw0311@gmail.com>
2018-10-17 06:41:33 +1100

    implement the mapManager- DFS

--- src/mycontroller/MyAIController.java
+++ src/mycontroller/MyAIController.java
@@ -13,26 +13,59 @@ import world.Car;
 public class MyAIController extends CarController{
 
 	private int view;
+	private Coordinate start;
+	private enum GoalDirection {North,South,West,East};
+	private GoalDirection currGoal;
+	
 	
 	public MyAIController(Car car) {
 		super(car);
 		//MapManager.getInstance().initialize(getMap(),getPosition(),getViewSquare(),getView());
 		MapManager.getInstance().initialize(this);
 		view = getViewSquare();
+		start = new Coordinate(getPosition());
+		currGoal = GoalDirection.North;
 	}
 
 	@Override
 	public void update() {
 		
-
+		
 		
 	}
 	
 	
 	public void ClockWiseSearch() {
-		for (int i=0;i<view;i++) {
+		int x = start.x;
+		int y = start.y;
+		Coordinate currPos = new Coordinate(getPosition());
+		switch(currGoal) {
+		case North:
 			
+//			if (currPos.y < y) {
+//				if (MapManager.getInstance().toNorth().isType(Type.ROAD)) {
+//
+//				}
+//			}
+//			else {
+//				currGoal = GoalDirection.East;
+//			}
+			
+			
+			
+			
+			break;
+		case East:
+			break;
+		case South:
+			break;
+		case West:
+			break;
+		default:
+			System.err.println("I am into default case");
+			break;
 		}
+		
 	}
 
 }
